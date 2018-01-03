2566684f1ef06ab17dbc53abd5341d3009bd3c62
miran <k1025mr@gmail.com>
2018-01-03 15:46:39 +0900

    오타수정

--- exAOP/src/main/java/com/hb/ex01/Ex01.java
+++ exAOP/src/main/java/com/hb/ex01/Ex01.java
@@ -8,7 +8,7 @@ import com.hb.model.GuestDao;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		ApplicationContext ac= new ClassPathXmlApplicationContext("/mybean.xml");
+		ApplicationContext ac= new ClassPathXmlApplicationContext("/myBean.xml");
 		// TODO Auto-generated method stub
 		GuestDao guestDao=(GuestDao) ac.getBean("guestDao");
 		guestDao.listDao();
